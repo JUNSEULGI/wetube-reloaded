3f772eb094107ad885c5d59962ca73cba8609f1d
전슬기 <jeonseulgi@jeonseulgiui-MacBook-Air.local>
2021-12-14 01:25:30 +0900

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # wetube-reloaded
+
+/ -> home
+/join -> join
+/login -> login
+/search -> search
+
+/users/:id -> see user
+/users/logout -> log out
+/users/edit -> edit my profile
+/users/delet -> delete my profile
+
+/videos/:id -> see video
+/videos/:id/edit -> edit video
+/videos/:id/delete -> delete video
+/videos/upload -> upload video
+
+comment
---
+++ src/controllers/userController.js
@@ -0,0 +1,7 @@
+export const join = (req, res) => res.send("Join");
+export const login = (req, res) => res.send("login");
+export const logout = (req, res) => res.send("logout");
+
+export const edit = (req, res) => res.send("Edit User");
+export const deleteUser = (req, res) => res.send("Delete User");
+export const see = (req, res) => res.send("see User");
